50932810062fb603a474e76afea1867595bad0d7
Kareem Mohamed <kareem3d.a@gmail.com>
2018-11-30 20:43:34 +0100

    Initializing repository with create-react-app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
+import 'normalize.css/normalize.css';
+
+WebFont.load({
+  google: {
+    families: ['Open+Sans:400,600,700'],
+  },
+});
+
