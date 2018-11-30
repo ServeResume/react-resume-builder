6c978db87860333b78cdc93479ea42fc4b469f58
Kareem Mohamed <kareem3d.a@gmail.com>
2018-11-30 20:45:16 +0100

    Adding dummy resume data

---
+++ src/resume/educations.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    title: 'B.Sc. in Computer Engineering',
+    location: 'University of Portsaid',
+    dateFrom: 'Sept 2009',
+    dateTo: 'Sept 2014',
+  },
+];
---
+++ src/resume/hobbies.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/24034-200.png',
+    title: 'Chess',
+  },
+  {
+    image: 'https://cdn4.iconfinder.com/data/icons/sports-recreation/128/ping-pong-2-512.png',
+    title: 'Table Tennis',
+  },
+  {
+    image: 'http://www.myiconfinder.com/uploads/iconsets/256-256-f6aff8f3d76291cf1054c1633e763ca2-bicycle.png',
+    title: 'Cycling',
+  },
+  {
+    image: 'https://d30y9cdsu7xlg0.cloudfront.net/png/2248-200.png',
+    title: 'Reading',
+  },
+];
